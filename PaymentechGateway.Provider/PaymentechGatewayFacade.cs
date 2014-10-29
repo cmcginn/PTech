1f8be51e670a27ef5720694587bb21bb8b3fbb55
Chris McGinn <chris.s.mcginn@live.com>
2014-10-29 09:54:47 -0400

    wip

--- PaymentechGateway.Provider/PaymentechGatewayFacade.cs
+++ PaymentechGateway.Provider/PaymentechGatewayFacade.cs
@@ -8,6 +8,11 @@ namespace PaymentechGateway.Provider
 {
     public class PaymentechGatewayFacade:IPaymentechGatewayFacade
     {
+        private readonly PaymentechGatewaySettings _settings;
+        public PaymentechGatewayFacade(PaymentechGatewaySettings settings)
+        {
+            _settings = settings;
+        }
         public ProfileResponse CreatePaymentechProfile()
         {
             throw new NotImplementedException();
--- PaymentechGateway.Tests/PaymentechGatewayFacadeTests.cs
+++ PaymentechGateway.Tests/PaymentechGatewayFacadeTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PaymentechGateway.Provider;
 
@@ -10,6 +11,17 @@ namespace PaymentechGateway.Tests
 
         IPaymentechGatewayFacade GetTarget()
         {
+            var settings = new PaymentechGatewaySettings();
+            settings.Bin = "001";
+            settings.Bin = "000001";
+            settings.TerminalId = "001";
+            settings.Username = ConfigurationManager.AppSettings["username"];
+            settings.Password = ConfigurationManager.AppSettings["password"];
+            settings.SandboxGatewayUrl = "https://wsvar.paymentech.net/PaymentechGateway";
+            settings.SandboxGatewayFailoverUrl = "https://wsvar2.paymentech.net/PaymentechGateway";
+            settings.MerchantId = ConfigurationManager.AppSettings["merchantid"];
+            settings.RecurringMerchantId = ConfigurationManager.AppSettings["recurringmerchantid"];
+            settings.UseSandbox = true;  
             return new PaymentechGatewayFacade();
         }
         [TestMethod]
